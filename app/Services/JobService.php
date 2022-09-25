d2d6ef0ddc9a7e83009440f44a21f4027de8a8ee
Kevin Figuera <kevinalejandro3@gmail.com>
2022-09-25 17:04:30 -0400

    Contenedor de inyección de dependencias

---
+++ app/Services/JobService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Job;
+
+class JobService
+{
+    public function deleteJob($id) {
+        $job = Job::find($id);
+        $job->delete();
+    }
+}
--- public/index.php
+++ public/index.php
@@ -11,9 +11,11 @@ session_start();
 $dotenv = new Dotenv\Dotenv(__DIR__ . '/..');
 $dotenv->load();
 
+
 use Illuminate\Database\Capsule\Manager as Capsule;
 use Aura\Router\RouterContainer;
 
+$container = new DI\Container();
 $capsule = new Capsule;
 
 $capsule->addConnection([
@@ -92,24 +94,6 @@ $map->get('admin', '/admin', [
 $matcher = $routerContainer->getMatcher();
 $route = $matcher->match($request);
 
-function printElement($job) {
-    // if($job->visible == false) {
-    //   return;
-    // }
-  
-    echo '<li class="work-position">';
-    echo '<h5>' . $job->title . '</h5>';
-    echo '<p>' . $job->description . '</p>';
-    echo '<p>' . $job->getDurationAsString() . '</p>';
-    echo '<strong>Achievements:</strong>';
-    echo '<ul>';
-    echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-    echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-    echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-    echo '</ul>';
-    echo '</li>';
-}
-
 if (!$route) {
     echo 'No route';
 } else {
@@ -124,7 +108,7 @@ if (!$route) {
         die;
     }
 
-    $controller = new $controllerName;
+    $controller = $container->get($controllerName);
     $response = $controller->$actionName($request);
 
     foreach($response->getHeaders() as $name => $values)
